77fb362bd8d50fb6081e0754fbab29248acd7e1a
wangzlls <943383786@qq.com>
2020-09-05 16:50:30 +0800

    '第一次提交'

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  // import {Swiper,SwiperItem} from 'components/common/swiper'
+  import Swiper from '../../../components/common/swiper/Swiper'
+  import SwiperItem from '../../../components/common/swiper/SwiperItem'
+
+  export default {
+    name: "HomeSwiper",
+    props:{
+      banners:{
+        type:Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components:{
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
